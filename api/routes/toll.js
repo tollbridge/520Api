20e7b228b329b75827bf1517fe001f504fae215c
ronbarrantes <ronbarrantes@gmail.com>
2019-11-17 22:57:49 -0800

    TO DELETE: add a temp route

--- api/routes/toll.js
+++ api/routes/toll.js
@@ -6,7 +6,11 @@ const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 const toolLink = process.env.TOLL_URI
 
+// TODO: DELETE tempSchedule
+let tempSchedule = {}
+
 module.exports = new Router()
+
   .get('/api/toll', (req, res, next)=>{
     const todayInfo = getTodayInfo()
     fetch(toolLink)
@@ -57,3 +61,27 @@ module.exports = new Router()
       .catch(next)
   })
 
+  // TODO: delete /api/toll/temp
+  .get('/api/toll/temp', (req, res, next)=>{
+    if(Object.keys(tempSchedule).length > 0){
+      console.log('I got the info cached')
+      const todayInfo = getTodayInfo()
+      res.json({todayInfo, tempSchedule})
+    }
+    else {
+      console.log('do not have the info cached')
+      const todayInfo = getTodayInfo()
+      fetch(toolLink)
+        .then(res => res.buffer())
+        .then(res => res.toString())
+        .then(html=>{
+
+          const $ = cheerio.load(html)
+          const rawTable = $('table').find('tbody')[Number(todayInfo.isWeekendOrHoliday)]
+          const todaySchedule = getParsedWebData(rawTable)
+          tempSchedule = todaySchedule
+          res.json({todayInfo, tempSchedule})
+        })
+        .catch(next)
+    }
+  })
